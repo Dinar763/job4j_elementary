f065bebc663f518de6cd42d38ac3056bf2322fc1
Dinar <gizzyatovdinar@gmail.com>
2024-09-11 14:28:01 +0400

    3.2. Загрузка файлов на github [#243582]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -24,7 +24,7 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         if (start <= finish) {
-            for (int i = 0; i <= finish ; i++) {
+            for (int i = 0; i <= finish; i++) {
                 if (i % 2 == 0) {
                     sum += i;
                 }
