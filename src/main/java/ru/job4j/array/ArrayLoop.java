86598b0dd44c6f2789d5947dbfee668cf180ea3a
Dinar <gizzyatovdinar@gmail.com>
2024-09-20 18:54:24 +0400

    3.2. Загрузка файлов на github [#243598]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -4,7 +4,7 @@ public class ArrayLoop {
     public static void main(String[] args) {
         int[] array = new int[5];
         for (int i = 0; i < array.length; i++) {
-            array[i] = i * 2 + 3;
+            array[i] = 3 + i * 2;
         }
         for (int j : array) {
             System.out.println(j);
