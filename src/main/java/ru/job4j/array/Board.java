1ad7ea4bebac4970c4273b7f0dfce8cf4b85d9b5
Dinar <gizzyatovdinar@gmail.com>
2024-09-21 13:32:40 +0400

    3.2. Загрузка файлов на github [#243604]

--- src/main/java/ru/job4j/array/Board.java
+++ src/main/java/ru/job4j/array/Board.java
@@ -8,7 +8,7 @@ public class Board {
     public static void paint(int width, int height) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if (j == i || ((j + i) % 2 == 0)) {
+                if ((j + i) % 2 == 0) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
