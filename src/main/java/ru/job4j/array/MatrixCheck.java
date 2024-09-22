c2b5b39fa02da983f28d991ffee3cdf8023f95b1
Dinar <gizzyatovdinar@gmail.com>
2024-09-22 12:22:48 +0400

    3.2. Загрузка файлов на github [#243622]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,4 +11,15 @@ public class MatrixCheck {
         }
         return result;
     }
+
+//    public static boolean monoVertical(char[][] board, int column) {
+//        boolean result = true;
+//        for () {
+//            if () {
+//                result = false;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
 }
