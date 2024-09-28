3ffe4edb8b832d66f1e1ba2b75dfd7f0c362bf60
Dinar <gizzyatovdinar@gmail.com>
2024-09-28 06:56:54 +0400

    3.2. Загрузка файлов на github [#243636]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,14 +2,11 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-        int[] targArray = new int[2];
         int i = 0;
         int j = array.length - 1;
         while (i < j) {
             if (array[i] + array[j] == target) {
-                targArray[0] = i;
-                targArray[1] = j;
-                return targArray;
+                return new int[]{i, j};
             } else if (array[i] + array[j] < target) {
                 i++;
             } else {
