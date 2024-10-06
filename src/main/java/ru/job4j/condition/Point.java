708705888d91e6f7b2977ee4c8379ff19eae1982
Dinar <gizzyatovdinar@gmail.com>
2024-10-06 07:19:58 +0400

    3.2. Загрузка файлов на github [#243639]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -7,20 +7,24 @@ public class Point {
 
     private int x;
     private int y;
-
-    public double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
-    }
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double distance = a.distance(b);
-        System.out.println(distance);
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 }
