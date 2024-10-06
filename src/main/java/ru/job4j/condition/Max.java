b3ee3c7cd505b03ee075a0af3bd738003018282e
Dinar <gizzyatovdinar@gmail.com>
2024-10-06 07:20:40 +0400

    3.2. Загрузка файлов на github [#243639]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,10 +6,10 @@ public class Max {
     }
 
     public static int max(int number1, int number2, int number3) {
-        return Math.max(max(number1, number2), number3);
+        return max(max(number1, number2), number3);
     }
 
     public static int max(int number1, int number2, int number3, int number4) {
-        return Math.max(max(number1, number2, number3), number4);
+        return max(max(number1, number2, number3), number4);
     }
 }
